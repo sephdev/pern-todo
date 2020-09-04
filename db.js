011dddaf6de1aea93172d11c0b933ee004935919
Joseph Tandingan <josephdevapps@gmail.com>
2020-09-05 02:23:29 +0800

    ready for deployment

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
+
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
+// };
+
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+
+// Heroku addons
+const proConfig = process.env.DATABASE_URL
 
 const pool = new Pool({
-  user: "josephtandingan",
-  password: "",
-  host: "localhost",
-  port: 5432,
-  database: "perntodo"
+  connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+// procecss.env.PORT
+// process.env.NODE_ENV => production or undefined
+
+if (process.env.NODE_ENV === "production") {
+  // server static content
+  // npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("./client/build"));
 
 // ROUTES
 
@@ -79,6 +96,10 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
